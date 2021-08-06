8275228b37d940d4297a9b2b3d3cadcd2090efa1
FirstNiwt <doofficialpr@gmail.com>
2021-08-06 17:27:27 +0200

    added alternative definition and error handling

--- DictionaryApp/app/src/main/java/com/example/dictionaryapp/MainActivity.kt
+++ DictionaryApp/app/src/main/java/com/example/dictionaryapp/MainActivity.kt
@@ -11,8 +11,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
-    private val key = "WORD_DEFINITION"
+    private val defKey = "WORD_DEFINITION"
+    private val secondDefKey = "SECOND_WORD_DEFINITION"
     private val idKey = "ID"
+    private val twoOrMoreKey = "MoreThanOne"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         val queue = Volley.newRequestQueue(this)
         search_button.setOnClickListener {
 
-            val word = word_searched.text
+            val word = word_searched.text.toString()
             val apiKey = "d7fbdb12-bfb0-455d-8d9f-0a5c9e0cd871"
             val url =
                 "https://www.dictionaryapi.com/api/v3/references/learners/json/$word?key=$apiKey"
@@ -30,7 +32,12 @@ class MainActivity : AppCompatActivity() {
                 url,
                 {
                                   response ->
-                    extractDefinitionFromJson(response)
+                    try {
+                        extractDefinitionFromJson(response,word)
+                    } catch (exception:Exception){
+                        Toast.makeText(this,"Word not in dictionary, enter a valid word!",Toast.LENGTH_SHORT).show()
+
+                    }
                 },
                 {
 
@@ -42,21 +49,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun extractDefinitionFromJson(response: String){
-
+    private fun extractDefinitionFromJson(response: String,text:String){
+        var moreThanOneDef = false
         val jsonArray = JSONArray(response)
 
         val firstIndex = jsonArray.getJSONObject(0)
         val arrayOfShortDef = firstIndex.getJSONArray("shortdef")
         val shortDef = arrayOfShortDef.get(0)
+        var alternateDefinition = ""
 
-        val arrayMeta = firstIndex.getJSONObject("hwi")
-        val searchId = arrayMeta.getString("hw")
-
+        if(arrayOfShortDef.length()>1) {
+            moreThanOneDef = true
+            alternateDefinition = arrayOfShortDef.get(1).toString()
+        }
 
         val intent = Intent(this, WordDefinitionActivity::class.java)
-        intent.putExtra(key,shortDef.toString())
-        intent.putExtra(idKey,searchId)
+        intent.putExtra(defKey,shortDef.toString())
+        intent.putExtra(idKey, text)
+        intent.putExtra(twoOrMoreKey,moreThanOneDef)
+        intent.putExtra(secondDefKey,alternateDefinition)
         startActivity(intent)
 
 
--- DictionaryApp/app/src/main/java/com/example/dictionaryapp/WordDefinitionActivity.kt
+++ DictionaryApp/app/src/main/java/com/example/dictionaryapp/WordDefinitionActivity.kt
@@ -1,20 +1,47 @@
 package com.example.dictionaryapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_word_definition.*
 
 class WordDefinitionActivity : AppCompatActivity() {
-    private val key = "WORD_DEFINITION"
+    private val defKey = "WORD_DEFINITION"
+    private val secondDefKey = "SECOND_WORD_DEFINITION"
     private val idKey = "ID"
+    private val twoOrMoreKey = "MoreThanOne"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_definition)
 
-        intent.getStringExtra(key)
-        short_def_view.text = intent.getStringExtra(key)
+
+        short_def_view.text = intent.getStringExtra(defKey)
         searched_word_view.text = intent.getStringExtra(idKey)
 
         back_to_main_view.setOnClickListener{ finish()}
+        if(intent.getBooleanExtra(twoOrMoreKey,false)) {
+            alternative_definition.setVisibility(View.VISIBLE)
+        }
+
+        alternative_definition.setOnClickListener{
+
+            if(alternative_definition.text == "SHOW ALTERNATIVE DEFINITION") {
+                short_def_view.text = intent.getStringExtra(secondDefKey)
+                alternative_definition.text = "SHOW MAIN DEFINITION"
+                alternative_definition.setBackgroundColor(Color.BLUE)
+            }
+
+            else {
+                short_def_view.text = intent.getStringExtra(defKey)
+                alternative_definition.text = "SHOW ALTERNATIVE DEFINITION"
+                alternative_definition.setBackgroundColor(resources.getColor(R.color.orange))
+
+            }
+
+        }
+
+        }
     }
-}
