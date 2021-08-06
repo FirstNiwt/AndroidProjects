eb0afb44ca404dd0756003c90b3fe2a5880a170a
FirstNiwt <doofficialpr@gmail.com>
2021-08-06 14:50:55 +0200

    connect with api and small fixes

--- DictionaryApp/app/src/main/java/com/example/dictionaryapp/MainActivity.kt
+++ DictionaryApp/app/src/main/java/com/example/dictionaryapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.dictionaryapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val queue = Volley.newRequestQueue(this)
+        search_button.setOnClickListener {
+
+            val word = word_searched.text
+            val apiKey = "d7fbdb12-bfb0-455d-8d9f-0a5c9e0cd871"
+            val url =
+                "https://www.dictionaryapi.com/api/v3/references/learners/json/$word?key=$apiKey"
+
+            val stringRequest = StringRequest(
+                Request.Method.GET,
+                url,
+                Response.Listener {
+                                  response ->
+                    response
+                },
+                Response.ErrorListener {
+
+                        error ->
+                    error
+                })
+
+            queue.add(stringRequest)
+        }
     }
 }
--- DictionaryApp/app/src/main/java/com/example/dictionaryapp/WordDefinitionActivity.kt
+++ DictionaryApp/app/src/main/java/com/example/dictionaryapp/WordDefinitionActivity.kt
@@ -7,5 +7,6 @@ class WordDefinitionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_definition)
+
     }
 }
