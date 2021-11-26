6b60a7741855552c0739828853d2d5e2dc9406f1
FirstNiwt <doofficialpr@gmail.com>
2021-11-26 02:37:01 +0100

    added moment view adapter and view holder logic

--- MyMoments/app/src/main/java/com/example/mymoments/MomentActivity.kt
+++ MyMoments/app/src/main/java/com/example/mymoments/MomentActivity.kt
@@ -2,10 +2,27 @@ package com.example.mymoments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_moment.*
+
 
 class MomentActivity : AppCompatActivity() {
+
+    private var momentList : ArrayList<Moment> = ArrayList() // list represeting all moments
+    private lateinit var linearLayoutManager: LinearLayoutManager // our listview Manager
+    private lateinit var adapter : MomentAdapter  //adapter for each moment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_moment)
+
+        momentList.add(Moment("2019","first Moment","My first moment"))
+        momentList.add(Moment("2020","second Moment","My second moment"))
+
+        linearLayoutManager = LinearLayoutManager(this)
+        recycler_view.layoutManager = linearLayoutManager
+        adapter = MomentAdapter(momentList)
+
+        recycler_view.adapter = adapter
     }
 }
--- MyMoments/app/src/main/java/com/example/mymoments/MomentAdapter.kt
+++ MyMoments/app/src/main/java/com/example/mymoments/MomentAdapter.kt
@@ -1,32 +1,58 @@
 package com.example.mymoments
 
+import android.text.Layout
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MomentAdapter(private var momentList: MutableList<Moment>) : RecyclerView.Adapter<MomentAdapter.MomentViewHolder> {
+class MomentAdapter(private var momentList: MutableList<Moment>) : RecyclerView.Adapter<MomentAdapter.MomentViewHolder>() {
     override fun onCreateViewHolder(
-        parent: ViewGroup,
+        viewGroup: ViewGroup,
         viewType: Int
     ): MomentAdapter.MomentViewHolder {
-        TODO("Not yet implemented")
+
+        val context = viewGroup.context
+        val inflater = LayoutInflater.from(context)
+        val attachToParentImmediately = false
+
+        val view = inflater.inflate(R.layout.recycler_moment_item, viewGroup, attachToParentImmediately)
+
+        return MomentViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MomentAdapter.MomentViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = momentList[position]
+        holder.attachMoment(item)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return momentList.size
+
     }
 
-    class MomentViewHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener{
+    class  MomentViewHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener{
 
 
         private var view: View = v
-        private lateinit var moment: MomentAdapter
-        init{
+        private lateinit var moment: Moment
+        private var date : TextView
+        private var title: TextView
+        init
+        {
+            date = view.findViewById(R.id.date_recycler_item)
+            title = view.findViewById(R.id.recycler_title_item)
             v.setOnClickListener(this)
+
+        }
+
+        fun attachMoment(moment: Moment)
+        {
+            this.moment = moment
+
+            date.text = moment.date
+            title.text = moment.title
         }
 
         override fun onClick(v: View?) {
